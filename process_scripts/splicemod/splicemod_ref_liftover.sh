8ec05f339b3c746226717fa4e59a985cd405df00
kinsigne <kiminsigne@gmail.com>
2017-08-17 19:53:42 -0700

    Initial commit

---
+++ process_scripts/splicemod/splicemod_ref_liftover.sh
@@ -0,0 +1,8 @@
+# Runs UCSC liftOver command to convert coordinates from hg19 to hg38
+# called in splicemod_format_ref.R and depends on file produced in first chunk
+
+liftOver <(cat ../../ref/splicemod/splicemod_ref_formatted.txt | \
+  awk '{OFS="\t"; print "chr"$4,$5,$6,$1}' | grep -v 'NA' | sort | uniq) \
+  ../../ref/hg19ToHg38.over.chain \
+  ../../processed_data/splicemod/splicemod_ref_liftover.bed \
+  ../../processed_data/splicemod/splicemod_unlifted.bed
