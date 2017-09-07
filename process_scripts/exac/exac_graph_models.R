0f66218fd122892d4466fabe74f31439d713ceb1
kinsigne <kiminsigne@gmail.com>
2017-09-07 16:05:53 -0700

    updated PR curves

--- process_scripts/exac/exac_graph_models.R
+++ process_scripts/exac/exac_graph_models.R
@@ -90,8 +90,8 @@ ggplot(roc_curve_info, aes(false_positive_rate, true_positive_rate)) +
 # ###############################################################################
 # scores by annotation category
 # ###############################################################################
-# data_annot <- read.table('../../processed_data/exac/exac_func_annot.txt',
-#                         sep = '\t', header = T)
+data_annot <- read.table('../../processed_data/exac/exac_func_annot.txt',
+                        sep = '\t', header = T)
 # 
 # hal <- read.table('../../processed_data/exac/exac_HAL_scores.txt',
 #                    sep = '\t', header = T)
@@ -103,21 +103,21 @@ ggplot(roc_curve_info, aes(false_positive_rate, true_positive_rate)) +
 #     left_join(select(hal, id, hal_dpsi = DPSI_pred), by = 'id') %>% 
 #     left_join(select(spanr, id, spanr_dpsi = dpsi_spanr_capped), by = 'id')
 # 
-# data_annot$consequence <- factor(data_annot$consequence, 
-#                                  levels = c('splice_acceptor_variant', 
-#                                             'splice_donor_variant',
-#                                             'stop_gained', 'missense_variant', 
-#                                             'synonymous_variant', 'intron_variant'))
-# 
-# annot_names <- c(
-#     'splice_acceptor_variant' = 'splice acceptor variant',
-#     'splice_donor_variant' = 'splice donor variant',
-#     'stop_gained' = 'stop gained variant',
-#     'missense_variant' = 'missense variant',
-#     'synonymous_variant' = 'synonymous variant',
-#     'intron_variant' = 'intron variant'
-# )
-# 
+data_annot$consequence <- factor(data_annot$consequence,
+                                 levels = c('splice_acceptor_variant',
+                                            'splice_donor_variant',
+                                            'stop_gained', 'missense_variant',
+                                            'synonymous_variant', 'intron_variant'))
+
+annot_names <- c(
+    'splice_acceptor_variant' = 'splice acceptor variant',
+    'splice_donor_variant' = 'splice donor variant',
+    'stop_gained' = 'stop gained variant',
+    'missense_variant' = 'missense variant',
+    'synonymous_variant' = 'synonymous variant',
+    'intron_variant' = 'intron variant'
+)
+
 # gg1 <- data_annot %>% 
 #     filter(!is.na(consequence)) %>% 
 #     ggplot(aes(v2_dpsi, noncoding_score)) + geom_point(alpha = 0.50) +
@@ -152,20 +152,20 @@ ggplot(roc_curve_info, aes(false_positive_rate, true_positive_rate)) +
 # plot_grid(gg1, gg2, nrow = 2)
 # 
 # 
-# gg1 <- data_annot %>% 
-#     filter(!is.na(consequence)) %>% 
+# gg1 <- data_annot %>%
+#     filter(!is.na(consequence)) %>%
 #     ggplot(aes(v2_dpsi, fitCons_score)) + geom_point(alpha = 0.50) +
 #     facet_wrap(~ consequence, labeller = as_labeller(annot_names)) +
 #     labs(x = expression(paste(Delta, ' inclusion index')),
 #          y = 'fitCons score')
 # 
 # 
-# gg2 <- data_annot %>% 
-#     filter(!is.na(consequence)) %>% 
+# gg2 <- data_annot %>%
+#     filter(!is.na(consequence)) %>%
 #     ggplot(aes(v2_dpsi, linsight_score)) + geom_point(alpha = 0.50) +
 #     facet_wrap(~ consequence, labeller = as_labeller(annot_names)) +
 #     labs(x = expression(paste(Delta, ' inclusion index')),
 #          y = 'LINSIGHT score')
-# 
+#  
 # plot_grid(gg1, gg2, nrow = 2)
 
